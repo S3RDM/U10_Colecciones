a8b3e064b21907275c18ecbffc2d0696e7688a67
S3RDM <legochecho@gmail.com>
2026-05-04 14:06:03 +0200

    A04

--- a04/A04.java
+++ a04/A04.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class A04 {
     public static void main(String[] args) {
         ArrayList<Integer> numeros = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for(int i = 0; i < 100; i++){
             int num = (int)(Math.random() * 10) + 1;
             numeros.add(num);
         }
@@ -12,8 +12,8 @@ public class A04 {
         System.out.println(numeros);
         
         Iterator<Integer> it = numeros.iterator();
-        while (it.hasNext()) {
-            if (it.next() == 5) {
+        while(it.hasNext()){
+            if (it.next() == 5){
                 it.remove();
             }
         }
