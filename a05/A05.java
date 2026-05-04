86defbfb8c774b781eb8d9db36a5534c197fb8e9
S3RDM <legochecho@gmail.com>
2026-05-04 14:10:43 +0200

    A05

--- a05/A05.java
+++ a05/A05.java
@@ -5,7 +5,6 @@ public class A05 {
     public static void main(String[] args){
         ArrayList<Integer> lista = new ArrayList<>();
         ArrayList<Integer> eliminar = new ArrayList<>();
-
         for(int i = 0; i < 100; i++){
             int num = (int)(Math.random() * 10) + 1;
             lista.add(num);
