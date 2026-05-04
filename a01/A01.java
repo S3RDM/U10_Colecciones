6bd752671d877813150a41f0477a0883cc92d9e3
S3RDM <legochecho@gmail.com>
2026-05-04 08:26:00 +0200

    A01

--- a01/A01.java
+++ a01/A01.java
@@ -3,6 +3,22 @@ import java.util.Arrays;
 
 public class A01 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] s = {"A1","A2"};
+        s = guardar("B1", s);
+        s = guardar("B2", s);
+        
+        System.out.println(Arrays.toString(s));
+        
+        Integer[] i = {2,3};
+        i = guardar(1, i);
+        i = guardar(2, i);
+        
+        System.out.println(Arrays.toString(i));
+    }
+    
+    public static <E> E[] guardar (E elemento,E tabla []){
+        tabla = Arrays.copyOf(tabla, tabla.length +1);
+        tabla[tabla.length-1]= elemento;
+        return tabla;
     }
 }
