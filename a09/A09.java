e1612cf67eeb9887ea65f768d698fc20186c5a05
S3RDM <legochecho@gmail.com>
2026-05-07 09:47:21 +0200

    A09

--- a09/A09.java
+++ a09/A09.java
@@ -9,13 +9,13 @@ public class A09 {
             lista.add(num);
         }
         System.out.println("Lista original: " + lista);
-        
+        //HashSet para que no haya repetidos y con un orden aleatorio
         Set<Integer> listaHash = new HashSet<>(lista);
         System.out.println("Lista sin repetidos: " + listaHash);
-        
+        //TreeSet para ordenar ascendentemente sin repetidos
         Set<Integer> listaTree = new TreeSet<>(lista);
         System.out.println("Lista con TreeSet: " + listaTree);
-        
+        //LinkedHashList para usar el orden de la lista original sin repetidos
         Set<Integer> listaLinked = new LinkedHashSet<>(lista);
         System.out.println("Lista con LinkedHashSet: " + listaLinked);
     }
