461a44e60d4316e9ad541d96c17b578b1b19dafc
S3RDM <legochecho@gmail.com>
2026-05-07 09:54:49 +0200

    A06

--- a06/A06.java
+++ a06/A06.java
@@ -8,24 +8,17 @@ public class A06 {
             int num = (int)(Math.random() * 100) + 1;
             lista.add(num);
         }
-
-        System.out.println("Lista original: ");
-        System.out.println(lista);
+        System.out.println("Lista original: " + lista);
 
         Integer[] array = lista.toArray(new Integer[0]);
-
         Arrays.sort(array);
-
         ArrayList<Integer> menorAMayor = new ArrayList<>(Arrays.asList(array));
 
-        System.out.println("\nLista ordenada de menor a mayor: ");
-        System.out.println(menorAMayor);
+        System.out.println("Lista ordenada de menor a mayor: " + menorAMayor);
 
         Arrays.sort(array, Collections.reverseOrder());
-
         ArrayList<Integer> mayorAMenor = new ArrayList<>(Arrays.asList(array));
 
-        System.out.println("\nLista ordenada de mayor a menor: ");
-        System.out.println(mayorAMenor);
+        System.out.println("Lista ordenada de mayor a menor: " + mayorAMenor);
     }
 }
