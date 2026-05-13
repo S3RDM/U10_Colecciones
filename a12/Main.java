5c82d2858891538ec3db73b1168015ce15dfec33
S3RDM <legochecho@gmail.com>
2026-05-13 09:58:42 +0200

    A12

---
+++ a12/Main.java
@@ -0,0 +1,24 @@
+package a12;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList<Integer> premios = new ArrayList<>(Arrays.asList(1000,50,500,50,200));
+        
+        int premios50 = Collections.frequency(premios, 50);
+        System.out.println("Cantidad de premios de 50: " + premios50);
+        
+        int premioMax = Collections.max(premios);
+        int premioMin = Collections.min(premios);
+        System.out.println("Premio maximo: " + premioMax + "\nPremio minimo: " + premioMin);
+        
+        Collections.shuffle(premios);
+        System.out.println("Lista desordenada: " + premios);
+        
+        Collections.sort(premios);
+        System.out.println("Lista ordenada: " + premios);
+        
+        int posicion500 = Collections.binarySearch(premios,500);
+        System.out.println("Posicion de 500: " + posicion500);
+    }
+}
